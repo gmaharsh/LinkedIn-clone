cd50a71d88eacad176bf9ba12f2b6909bd1d17c0
Maharsh <gmaharsh@yahoo.com>
2020-12-21 14:26:54 -0600

    Added Sidebar__Body

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './Components/Header/Header';
+import Sidebar from './Components/Body/Sidebar/Sidebar';
+import Feed from './Components/Body/Feed/Feed';
 
 function App() {
   return (
     <div className="app">
       {/* Header */}
       <Header />
+      <div className="app__body">
+        <Sidebar />
+        <Feed />
+      </div>
     </div>
   );
 }

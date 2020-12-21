42b712003d397afa1f93c2d581d47ffe949f9ba8
Maharsh <gmaharsh@yahoo.com>
2020-12-21 14:51:36 -0600

    Added Input for Feed Container

--- src/Components/Body/Feed/Feed.js
+++ src/Components/Body/Feed/Feed.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import CreateIcon from '@material-ui/icons/Create';
 import './Feed.css';
+import InputOptions from './InputOptions/InputOptions';
+import ImageIcon from '@material-ui/icons/Image';
+import { CalendarViewDay, EventNote, Subscriptions } from '@material-ui/icons';
 
 function Feed() {
     return (
@@ -13,7 +16,28 @@ function Feed() {
                         <button type="submit">Send</button>
                     </form>
                 </div>
+                <div className="feed__inputOptions">
+                    {/* <h1>Maharsh</h1> */}
+                    <InputOptions
+                        title="Photo"
+                        Icon={ImageIcon}
+                        color="#70B5F9" />
+                    <InputOptions
+                        title="Video"
+                        Icon={Subscriptions}
+                        color="#E7A33E" />
+                    <InputOptions
+                        title="Event"
+                        Icon={EventNote}
+                        color="#C0CBCD" />
+                    <InputOptions
+                        title="Write Article"
+                        Icon={CalendarViewDay}
+                        color="#7FC15E" />
+                </div>
             </div>
+
+            {/* Post */}
         </div>
     )
 }
